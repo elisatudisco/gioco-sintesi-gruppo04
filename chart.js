106bc39f0db13ac3b56c06200661532dd06ed4da
elisatudisco <elisa.tudisco@icloud.com>
2022-12-20 22:55:34 +0100

    spiegazione classifica

--- chart.js
+++ chart.js
@@ -1,31 +1,25 @@
-let risultati = [];
-let i = 0;
-let j = 0;
+// pag1
+// preparo la querystring
+let utente = "Gianni";
+window.location = "/pag2?currentUser=" + user;
 
-function setup() {
-    //risultati = getItem('chart');
-    createCanvas(400, 400);
-    if (risultati === null) {
-        risultati = [];
-    }
+// pag 2
+//ricavo il parametro dalla querystring
+const urlString = window.location.href;
+const url = new URL(urlString);
+user = url.searchParams.get("currentUser");
+console.log(user);
 
-    // let pippo = sessionStorage.getItem('test-item');
-    // console.log(pippo)
-    //risultati.push(pippo);
-    //storeItem('chart', risultati);
-}
+//faccio la partita
+let score = 123;
 
-function draw() {
-    background(255);
-    // text(risultati, 100, 100);
-    risultati.sort(function (a, b) { return a - b });
-}
+// creo un nuovo oggetto da caricare su JSON bin
+let userSession = {
+    'user': user,
+    'score': score
+};
 
-// function keyPressed() {
-//     if (key === "c") {
-//         sessionStorage.clear();
-//     }
-// }
+// scaricare tutto il record da json bin
 
 let BIN_ID = '6385e5b3a3c728450eda7d89';
 let API_KEY = '$2b$10$KWxVu8aW.YRXOvS0UwlDP.hS6u52PsWX9b69W6JfL6fvdiXoXqqde';
@@ -50,13 +44,50 @@ req.onreadystatechange = () => {
         req_u.setRequestHeader("Content-Type", "application/json");
         req_u.setRequestHeader("X-Master-Key", API_KEY);
 
-        if (data.zxc === undefined) {
-            data.zxc = [];
+        if (data.leaderboard === undefined) {
+            data.leaderboard = [];
         } else {
-            data.zxc.push(risultati);
+            data.zxc.push(userSession);
         }
 
         req_u.send(JSON.stringify(data));
+    }
+};
+
+req.open("GET", `https://api.jsonbin.io/v3/b/${BIN_ID}`, true);
+req.setRequestHeader("X-Master-Key", API_KEY);
+req.send();
+
+// pag. 3
+BIN_ID = '6385e5b3a3c728450eda7d89';
+API_KEY = '$2b$10$KWxVu8aW.YRXOvS0UwlDP.hS6u52PsWX9b69W6JfL6fvdiXoXqqde';
+
+let req = new XMLHttpRequest();
+
+req.onreadystatechange = () => {
+    if (req.readyState == XMLHttpRequest.DONE) {
+        function bubbleSort(arr) {
+            var len = arr.length;
+            for (var i = len - 1; i >= 0; i--) {
+                for (var j = 1; j <= i; j++) {
+                    if (arr[j - 1] > arr[j]) {
+                        var temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+        let data = JSON.parse(req.responseText).record;
+        let leaderboard = data.leaderboard;
+        leaderboard = bubbleSort(leaderboard);
+        console.log(leaderboard);
+
+        /// qui è dove metti su schermo i dati recuperati da JSON bin
+        leaderboard.forEach(element => {
+            // crei un nodo HTML con user e score
+        });
     }
 };
 
